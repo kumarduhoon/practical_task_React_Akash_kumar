590d9af9ab43b6ee684efcfc37bfd73f699fce66
Akash Kumar <akash.kumar@arkasoftwares.com>
2024-06-18 16:03:56 +0530

    project set up

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import Register from './pages/userauth/register/Register'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>React Js Test</h1>
+      <Register />
+    </>
+  )
+}
+
+export default App
